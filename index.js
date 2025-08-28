1a0e0f52c39213a573305b771cb76cccfc2b9453
ANDRE LUIZ FRANCA BATISTA <01411769627@pedag.local>
2025-08-28 14:36:40 -0300

    Aula do dia 28/08/2025

--- index.js
+++ index.js
@@ -21,6 +21,22 @@ app.get('/', (req, res) => {
 //Rotas
 app.use('/alunos', alunosRouter);
 
+//Handler de erros
+app.use((err, req, res, next) => {
+    console.error(`Erro: ${err}`);
+    //CastError
+    if (err.name === "CastError"){
+        return res.status(400).json({ erro: "ID inválido" });
+    }
+
+    //Erro de validação
+    if (err.name === "ValidationError"){
+        return res.status(400).json({ erro: "Validação falhou", detalhes: err.errors});
+    }
+
+    res.status(500).json({ erro: "Erro interno do servidor" });
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
--- routes/alunos.routes.js
+++ routes/alunos.routes.js
@@ -16,10 +16,64 @@ router.post('/', async (req, res, next) => {
     }
 });
 
-//GET
+//GET (lista de registros)
+//nome: "André Luiz França Batista"
+
+router.get('/', async (req, res, next) => {
+    try {
+        const { nome } = req.query;
+        const filtro = {};
+        if (nome) {
+            filtro.nome = new RegExp(nome, "i");
+        }
+        const alunos = await Aluno.find(filtro).sort({ createdAt: -1 });
+        return res.json(alunos);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//GET (único registro)
+router.get('/:id', async (req, res, next) => {
+    try {
+        const aluno = await Aluno.findById(req.params.id);
+        if (!aluno) {
+            return res.status(404).json({ erro: "Registro não encontrado." });
+        }
+        return res.json(aluno);
+    } catch (error) {
+        next(error);
+    }
+});
 
 //UPDATE
+router.put('/:id', async (req, res, next) => {
+    try {
+        const aluno = await Aluno.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if (!aluno) {
+            return res.status(404).json({ erro: "Aluno não encontrado para update." });
+        }
+        return res.json(aluno);
+    } catch (error) {
+        next(error);
+    }
+});
 
 //DELETE
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const aluno = await Aluno.findByIdAndDelete(req.params.id);
+        if(!aluno){
+            return res.status(404).json({ erro: "Aluno não encontrado para remoção"});
+        }
+        return res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
+})
+
 
 module.exports = router;
